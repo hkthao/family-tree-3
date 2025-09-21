4aa224fbe9d632f417708e7ef116bfb9f3f1d53e
Kim Thao Huynh <mrthao20111990@gmail.com>
2025-09-21 14:29:15 +0700

    fix lint

--- frontend/src/components/common/FamilyAutocomplete.vue
+++ frontend/src/components/common/FamilyAutocomplete.vue
@@ -1,7 +1,7 @@
 <template>
   <v-autocomplete
     :model-value="modelValue"
-    @update:model-value="updateModelValue('update:modelValue', $event)"
+    @update:model-value="$emit('update:modelValue', $event)"
     :items="families"
     item-title="name"
     item-value="id"
@@ -11,8 +11,8 @@
     :clearable="clearable"
     :custom-filter="familyFilter"
   >
-    <template #item="{ item }">
-      <v-list-item :title="item.raw.name" :subtitle="item.raw.address"></v-list-item>
+    <template #item="{ props, item }">
+      <v-list-item v-bind="props" :subtitle="item.raw.address"></v-list-item>
     </template>
   </v-autocomplete>
 </template>
@@ -22,15 +22,13 @@ import { ref, onMounted } from 'vue';
 import { useFamilies } from '@/data/families';
 import type { Family } from '@/types/family';
 
-const { modelValue, label, rules, readOnly, clearable } = defineProps<{
-  modelValue: string | number | null | undefined; // The selected family ID
+ defineProps<{
+  modelValue: any; // The selected family ID
   label?: string;
-  rules?: Array<(value: unknown) => boolean | string>;
+  rules?: Array<any>;
   readOnly?: boolean;
   clearable?: boolean;
 }>();
-
-const updateModelValue = defineEmits(['update:modelValue']);
 
 const { getFamilies } = useFamilies();
 const families = ref<Family[]>([]);
@@ -44,16 +42,15 @@ onMounted(async () => {
   families.value = fetchedFamilies;
 });
 
-const familyFilter = (_value: string, query: string, item: VuetifyInternalItem | undefined) => {
+const familyFilter = (_value: string, query: string, item: VuetifyInternalItem | undefined ) => {
   if (!item || !item.raw) return false;
 
   const rawItem = item.raw;
 
   const name = rawItem.name ? String(rawItem.name).toLowerCase() : '';
   const address = rawItem.address ? String(rawItem.address).toLowerCase() : '';
-  const id = String(rawItem.id);
   const searchText = query ? String(query).toLowerCase() : '';
 
-  return name.includes(searchText) || address.includes(searchText) || id.includes(searchText);
+  return name.includes(searchText) || address.includes(searchText);
 };
 </script>
