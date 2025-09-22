2cabfc8d604ab8af9e62c1afa215d7f40308a439
Kim Thao Huynh <mrthao20111990@gmail.com>
2025-09-22 08:19:20 +0700

    refactor

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -16,7 +16,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 
-    'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': 'warn',
     'vue/no-unused-components': 'warn',
     'vue/multi-word-component-names': 'off',
--- frontend/src/stores/utils.ts
+++
@@ -1,7 +0,0 @@
-export const simulateAsyncOperation = <T>(data: T, delay = 300): Promise<T> => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(data);
-    }, delay);
-  });
-};
