c817494fe9daad2b4a5460c3011445ca0c841fae
Kim Thao Huynh <mrthao20111990@gmail.com>
2025-10-05 11:27:10 +0700

    refactor: Remove GetMembersWithPaginationQuery and its handler

--- backend/src/Application/Members/Queries/GetMembers/GetMembersWithPaginationQuery.cs
+++
@@ -1,9 +0,0 @@
-using backend.Application.Common.Models;
-
-namespace backend.Application.Members.Queries.GetMembers;
-
-public record class GetMembersWithPaginationQuery : PaginatedQuery, IRequest<Result<PaginatedList<MemberListDto>>>
-{
-    public string? SearchTerm { get; init; }
-    public Guid? FamilyId { get; init; }
-}
