b522a274a90ec688b20be2668487e053e2a38aee
Kim Thao Huynh <mrthao20111990@gmail.com>
2025-10-05 13:13:04 +0700

    chore: Manual fixes

--- frontend/src/types/family/index.ts
+++ frontend/src/types/family/index.ts
@@ -1,3 +1,6 @@
 export * from './family';
 export * from './member';
 export * from './family-visibility';
+export * from './relationship';
+export * from './relationship-type';
+export * from './relationship-filter';
