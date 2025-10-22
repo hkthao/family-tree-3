6f48e7b74e6493fdc874104aad1a171abab148b9
Kim Thao Huynh <mrthao20111990@gmail.com>
2025-10-22 10:08:12 +0700

    feat: Add DeleteSystemConfigurationCommandValidator and its unit tests

---
+++ src/backend/src/Application/SystemConfigurations/Commands/DeleteSystemConfiguration/DeleteSystemConfigurationCommandValidator.cs
@@ -0,0 +1,12 @@
+using FluentValidation;
+
+namespace backend.Application.SystemConfigurations.Commands.DeleteSystemConfiguration;
+
+public class DeleteSystemConfigurationCommandValidator : AbstractValidator<DeleteSystemConfigurationCommand>
+{
+    public DeleteSystemConfigurationCommandValidator()
+    {
+        RuleFor(v => v.Id)
+            .NotEmpty().WithMessage("Id is required.");
+    }
+}
