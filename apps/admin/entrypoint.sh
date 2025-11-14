d5d1612181834fd20e5213c675dbaaffa424a5af
Kim Thao Huynh <mrthao20111990@gmail.com>
2025-11-14 07:49:33 +0700

    Cập nhật cấu hình Docker cho ứng dụng admin

--- apps/admin/entrypoint.sh
+++ apps/admin/entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-# infra/entrypoint.sh
 
-# Use envsubst to replace variables in the template file
-# The list of variables to substitute is passed as an argument to envsubst
-envsubst '${VITE_API_BASE_URL},${VITE_AUTH0_DOMAIN},${VITE_AUTH0_CLIENT_ID},${VITE_AUTH0_AUDIENCE},${VITE_NOVU_APPLICATION_IDENTIFIER},${VITE_APP_BUILD_DATE},${VITE_APP_ENVIRONMENT},${VITE_APP_COMMIT_ID}' < /usr/share/nginx/html/config.template.js > /usr/share/nginx/html/config.js
+VARS='${VITE_API_BASE_URL} ${VITE_AUTH0_DOMAIN} ${VITE_AUTH0_CLIENT_ID} ${VITE_AUTH0_AUDIENCE} ${VITE_NOVU_APPLICATION_IDENTIFIER} ${VITE_APP_BUILD_DATE} ${VITE_APP_ENVIRONMENT} ${VITE_APP_COMMIT_ID}'
 
-# Execute the original command (nginx)
+envsubst "$VARS" < /usr/share/nginx/html/config.template.js > /usr/share/nginx/html/config.js
+
 exec "$@"
