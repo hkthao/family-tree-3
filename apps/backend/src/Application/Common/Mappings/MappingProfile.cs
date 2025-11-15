43ae8506588c4bf252a525e51b865646ba92b763
Kim Thao Huynh <mrthao20111990@gmail.com>
2025-11-15 18:34:17 +0700

    fix(backend): Add missing using statement for RelationshipType in MappingProfile

--- apps/backend/src/Application/Common/Mappings/MappingProfile.cs
+++ apps/backend/src/Application/Common/Mappings/MappingProfile.cs
@@ -14,6 +14,7 @@ using backend.Application.UserActivities.Queries;
 using backend.Application.UserPreferences.Queries;
 using backend.Application.Users.Queries;
 using backend.Domain.Entities;
+using backend.Domain.Enums;
 
 namespace backend.Application.Common.Mappings;
 
@@ -35,7 +36,19 @@ public class MappingProfile : Profile
         CreateMap<Member, MemberListDto>()
             .ForMember(dest => dest.FamilyName, opt => opt.MapFrom(src => src.Family != null ? src.Family.Name : null))
             .ForMember(dest => dest.SourceRelationships, opt => opt.MapFrom(src => src.SourceRelationships))
-            .ForMember(dest => dest.TargetRelationships, opt => opt.MapFrom(src => src.TargetRelationships));
+            .ForMember(dest => dest.TargetRelationships, opt => opt.MapFrom(src => src.TargetRelationships))
+            .ForMember(dest => dest.FatherFullName, opt => opt.MapFrom(src => src.TargetRelationships.FirstOrDefault(r => r.Type == RelationshipType.Father && r.SourceMember != null)!.SourceMember!.FullName))
+            .ForMember(dest => dest.FatherAvatarUrl, opt => opt.MapFrom(src => src.TargetRelationships.FirstOrDefault(r => r.Type == RelationshipType.Father && r.SourceMember != null)!.SourceMember!.AvatarUrl))
+            .ForMember(dest => dest.MotherFullName, opt => opt.MapFrom(src => src.TargetRelationships.FirstOrDefault(r => r.Type == RelationshipType.Mother && r.SourceMember != null)!.SourceMember!.FullName))
+            .ForMember(dest => dest.MotherAvatarUrl, opt => opt.MapFrom(src => src.TargetRelationships.FirstOrDefault(r => r.Type == RelationshipType.Mother && r.SourceMember != null)!.SourceMember!.AvatarUrl))
+            .ForMember(dest => dest.HusbandFullName, opt => opt.MapFrom(src => src.SourceRelationships.FirstOrDefault(r => r.Type == RelationshipType.Husband && r.TargetMember != null)!.TargetMember!.FullName))
+            .ForMember(dest => dest.HusbandAvatarUrl, opt => opt.MapFrom(src => src.SourceRelationships.FirstOrDefault(r => r.Type == RelationshipType.Husband && r.TargetMember != null)!.TargetMember!.AvatarUrl))
+            .ForMember(dest => dest.WifeFullName, opt => opt.MapFrom(src => src.SourceRelationships.FirstOrDefault(r => r.Type == RelationshipType.Wife && r.TargetMember != null)!.TargetMember!.FullName))
+            .ForMember(dest => dest.WifeAvatarUrl, opt => opt.MapFrom(src => src.SourceRelationships.FirstOrDefault(r => r.Type == RelationshipType.Wife && r.TargetMember != null)!.TargetMember!.AvatarUrl))
+            .ForMember(dest => dest.FatherGender, opt => opt.MapFrom(src => src.TargetRelationships.FirstOrDefault(r => r.Type == RelationshipType.Father && r.SourceMember != null)!.SourceMember!.Gender))
+            .ForMember(dest => dest.MotherGender, opt => opt.MapFrom(src => src.TargetRelationships.FirstOrDefault(r => r.Type == RelationshipType.Mother && r.SourceMember != null)!.SourceMember!.Gender))
+            .ForMember(dest => dest.HusbandGender, opt => opt.MapFrom(src => src.SourceRelationships.FirstOrDefault(r => r.Type == RelationshipType.Husband && r.TargetMember != null)!.TargetMember!.Gender))
+            .ForMember(dest => dest.WifeGender, opt => opt.MapFrom(src => src.SourceRelationships.FirstOrDefault(r => r.Type == RelationshipType.Wife && r.TargetMember != null)!.TargetMember!.Gender));
         CreateMap<Member, MemberDetailDto>();
         CreateMap<Event, EventListDto>();
         CreateMap<Event, EventDetailDto>()
