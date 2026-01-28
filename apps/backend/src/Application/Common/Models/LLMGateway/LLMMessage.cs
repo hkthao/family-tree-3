4433136a4ce61c9a622d633e01f03a9cc7ea8249
Thao <thao@email.com>
2026-01-28 22:41:59 +0700

    fix bugs

--- apps/backend/src/Application/Common/Models/LLMGateway/LLMMessage.cs
+++
@@ -1,7 +0,0 @@
-namespace backend.Application.Common.Models.LLMGateway;
-
-public class LLMMessage
-{
-    public string Role { get; set; } = null!;
-    public string Content { get; set; } = null!;
-}
